3a3e7183cc5e5ad3e339849c75980190cdcfb7d7
sayali2202 <24ingles_1@rbunagpur.in>
2025-10-07 21:14:11 +0530

    Update car.java

--- car.java
+++ car.java
@@ -1,23 +1,22 @@
-// Car.java
+
 public class Car {
     String brand;
     int year;
 
-    // Default Constructor
+    
     public Car() {
         brand = "Unknown";
         year = 0;
         System.out.println("Default Constructor Called");
     }
 
-    // Parameterized Constructor
     public Car(String brand, int year) {
         this.brand = brand;
         this.year = year;
         System.out.println("Parameterized Constructor Called");
     }
 
-    // Method Overloading: displayInfo
+   
     public void displayInfo() {
         System.out.println("Brand: " + brand + ", Year: " + year);
     }
@@ -26,35 +25,35 @@ public class Car {
         System.out.println(message + " - Brand: " + brand + ", Year: " + year);
     }
 
-    // Destructor-like method (Java doesn't support destructors like C++, but finalize() simulates it)
+   
     @Override
     protected void finalize() throws Throwable {
         System.out.println("Car object is being destroyed");
     }
 
-    // Main method to test the functionality
     public static void main(String[] args) {
-        // Using default constructor
+       
         Car car1 = new Car();
         car1.displayInfo();
 
-        // Using parameterized constructor
+        
         Car car2 = new Car("Toyota", 2021);
         car2.displayInfo();
         car2.displayInfo("Car Info");
 
-        // Nullify object and request garbage collection
+        
         car1 = null;
         car2 = null;
 
-        // Requesting garbage collection
+       
         System.gc();
 
-        // Small pause to allow garbage collection to complete
+       
         try {
-            Thread.sleep(1000); // 1 second
+            Thread.sleep(1000); 
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 }
+
