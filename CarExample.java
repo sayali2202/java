4e3c7c2dc3804d9615ae1797c508ff384b524c29
sayali2202 <24ingles_1@rbunagpur.in>
2025-10-07 21:16:04 +0530

    Update CarExample.java

--- CarExample.java
+++ CarExample.java
@@ -1,20 +1,19 @@
 public class CarExample {
 
-    // Car class definition
+   
     static class Car {
-        // Fields (properties)
+   
         String brand;
         String color;
         int speed;
 
-        // Constructor
+   
         public Car(String brand, String color, int speed) {
             this.brand = brand;
             this.color = color;
             this.speed = speed;
         }
 
-        // Methods (behaviors)
         public void startEngine() {
             System.out.println(brand + " engine started.");
         }
@@ -29,21 +28,21 @@ public class CarExample {
         }
     }
 
-    // Main method
     public static void main(String[] args) {
-        // Creating objects of Car
+    
         Car car1 = new Car("Toyota", "Red", 50);
         Car car2 = new Car("Honda", "Black", 60);
 
-        // Using object methods
+
         car1.startEngine();
         car1.accelerate();
         car1.displayDetails();
 
-        System.out.println(); // space between cars
+        System.out.println(); 
 
         car2.startEngine();
         car2.accelerate();
         car2.displayDetails();
     }
 }
+
