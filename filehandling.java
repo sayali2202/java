198190d8796adec3bb0eb2079047c46b8e08be33
sayali2202 <24ingles_1@rbunagpur.in>
2025-10-07 21:17:13 +0530

    Update filehandling.java

--- filehandling.java
+++ filehandling.java
@@ -6,20 +6,20 @@ import java.io.IOException;
 public class FileHandlingExample {
 
     public static void main(String[] args) {
-        // Create a new file or open an existing one
+       
         File file = new File("example.txt");
 
         try {
-            // Create a FileWriter and BufferedWriter for writing to the file
+          
             FileWriter fileWriter = new FileWriter(file);
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
 
-            // Write data to the file
+     
             bufferedWriter.write("Hello, this is a test file.");
-            bufferedWriter.newLine();  // Adds a new line
+            bufferedWriter.newLine();  
             bufferedWriter.write("This is another line of text.");
             
-            // Close the BufferedWriter to save the content
+        
             bufferedWriter.close();
             System.out.println("Data written to the file successfully!");
 
@@ -29,3 +29,4 @@ public class FileHandlingExample {
         }
     }
 }
+
